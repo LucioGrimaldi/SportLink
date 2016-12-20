71a9d807f6003117958c9ec129eacd0c76a0b83c
LuciusFx <lucio.stig@gmail.com>
2016-12-20 10:24:56 +0100

    aggiungo pacchetto ui

--- app/src/main/java/com/project/is/sportlink/ui/HomeActivity.java
+++ app/src/main/java/com/project/is/sportlink/ui/HomeActivity.java
@@ -1,4 +1,4 @@
-package com.project.is.sportlink;
+package com.project.is.sportlink.ui;
 
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
@@ -12,6 +12,8 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+
+import com.project.is.sportlink.R;
 
 public class HomeActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
