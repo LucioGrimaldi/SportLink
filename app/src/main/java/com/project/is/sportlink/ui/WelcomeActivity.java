c9c53b0bba4f02ee6bedb3b9f24424f6d2b27be5
LuciusFx <lucio.stig@gmail.com>
2016-12-20 11:48:53 +0100

    inizio construzione della schermata di benvenuto e relativi layout

---
+++ app/src/main/java/com/project/is/sportlink/ui/WelcomeActivity.java
@@ -0,0 +1,10 @@
+package com.project.is.sportlink.ui;
+
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by luciogrimaldi on 20/12/16.
+ */
+
+public class WelcomeActivity extends AppCompatActivity {
+}
