b69b06dc09115661e5e636ac0e72cdcc3c7691fa
LuciusFx <lucio.stig@gmail.com>
2016-12-20 16:57:47 +0100

    Versione 1.0 della Welcome Page e impostati correttamente i font in main>assets>fonts

--- app/src/main/java/com/project/is/sportlink/ui/WelcomeActivity.java
+++ app/src/main/java/com/project/is/sportlink/ui/WelcomeActivity.java
@@ -1,19 +1,38 @@
 package com.project.is.sportlink.ui;
 
+import android.graphics.Typeface;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
 
 import com.project.is.sportlink.R;
 
 /**
  * Created by luciogrimaldi on 20/12/16.
+ * Version 1.0
  */
 
 public class WelcomeActivity extends AppCompatActivity {
+
+    TextView welcomeMessageTextView;
+    TextView welcomeAnswerMesssageTextView;
+    TextView userTextView;
+    TextView managerTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_welcome);
+        welcomeMessageTextView = (TextView) findViewById(R.id.welcome_message_text_view);
+        welcomeAnswerMesssageTextView = (TextView) findViewById(R.id.welcome_answer_message_text_view);
+        userTextView = (TextView) findViewById(R.id.user_text_view);
+        managerTextView = (TextView) findViewById(R.id.manager_text_view);
+
+        //Set custom fonts
+        Typeface RobotoThinFont = Typeface.createFromAsset(getAssets(), "fonts/Roboto-Thin.ttf");
+        welcomeMessageTextView.setTypeface(RobotoThinFont);
+        welcomeAnswerMesssageTextView.setTypeface(RobotoThinFont);
+        userTextView.setTypeface(RobotoThinFont);
+        managerTextView.setTypeface(RobotoThinFont);
     }
 }
