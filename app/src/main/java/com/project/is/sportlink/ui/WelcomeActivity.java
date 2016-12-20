b9b791f12dafccb70cbadb0574d57b6820fe2bdf
LuciusFx <lucio.stig@gmail.com>
2016-12-20 15:32:04 +0100

    aggiunta roboto font

--- app/src/main/java/com/project/is/sportlink/ui/WelcomeActivity.java
+++ app/src/main/java/com/project/is/sportlink/ui/WelcomeActivity.java
@@ -1,8 +1,9 @@
 package com.project.is.sportlink.ui;
 
 import android.os.Bundle;
-import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+
+import com.project.is.sportlink.R;
 
 /**
  * Created by luciogrimaldi on 20/12/16.
