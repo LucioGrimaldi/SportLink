3468279f8e5096e9aaee3b9aff6df008df820282
MarioPicciuolo <mar3pic@gmail.com>
2017-01-05 19:28:22 +0100

    ho inserito la libreria per l'utilizzo di microsoft azure

--- app/src/main/java/com/project/is/sportlink/logic/LoginController.java
+++ app/src/main/java/com/project/is/sportlink/logic/LoginController.java
@@ -1,8 +1,10 @@
 package com.project.is.sportlink.logic;
 
+import com.project.is.sportlink.ui.HomeActivity;
+
 /**
  * Created by luciogrimaldi on 22/12/16.
  */
-
 public class LoginController {
+
 }
