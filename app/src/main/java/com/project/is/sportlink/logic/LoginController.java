b3ba3ec556a60f8269e943c1fb95c242b8acae00
MarioPicciuolo <mar3pic@gmail.com>
2017-01-10 15:37:19 +0100

    sono stati completati i metodi per la login e tutta la funzione che permette ad un utente di effettuare la login funziona perfettamente.Aggiunto anche il pacchetto DataModel e il Gestore

--- app/src/main/java/com/project/is/sportlink/logic/LoginController.java
+++ app/src/main/java/com/project/is/sportlink/logic/LoginController.java
@@ -1,23 +1,19 @@
 package com.project.is.sportlink.logic;
 
-import android.app.AlertDialog;
 import android.app.ProgressDialog;
 import android.content.Context;
-import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.AsyncTask;
 import android.os.Build;
-import android.util.Log;
-import android.view.WindowManager;
 import android.widget.Toast;
 
 import com.microsoft.windowsazure.mobileservices.MobileServiceClient;
 import com.microsoft.windowsazure.mobileservices.MobileServiceException;
 import com.microsoft.windowsazure.mobileservices.http.OkHttpClientFactory;
 import com.microsoft.windowsazure.mobileservices.table.MobileServiceTable;
-import com.project.is.sportlink.Utente;
+import com.project.is.sportlink.DataModel.Gestore;
+import com.project.is.sportlink.DataModel.Utente;
 import com.project.is.sportlink.ui.HomeActivity;
-import com.project.is.sportlink.ui.LoginActivity;
 import com.squareup.okhttp.OkHttpClient;
 
 import java.net.MalformedURLException;
@@ -34,6 +30,7 @@ public class LoginController {
 
     private MobileServiceClient mClient;
     private MobileServiceTable<Utente> mUtenteTable;
+    private MobileServiceTable<Gestore> mGestoreTable;
     private Context context;
 
     public LoginController(Context context) {
@@ -41,7 +38,7 @@ public class LoginController {
     }
 
     //connessione DB
-    public void LoginRequest(String email, String password){
+    public void LoginRequest(String email, String password,boolean IS_UTENTE){
 
         try {
             // Create the Mobile Service Client instance, using the provided
@@ -61,12 +58,17 @@ public class LoginController {
                 }
             });
 
-            // Get the Mobile Service Table instance to use
-            mUtenteTable = mClient.getTable("utente",Utente.class);
-
-            //controllo se i dati inseriti sono corretti e se esiste l'utente con email e password inserite negli appositi campi
-            controlloDatiLogin(email,password);
-
+            if(IS_UTENTE) {
+                // Get the Mobile Service Table instance to use
+                mUtenteTable = mClient.getTable("utente", Utente.class);
+                //controllo se i dati inseriti sono corretti e se esiste l'utente con email e password inserite negli appositi campi
+                controlloDatiLoginUtente(email, password);
+            }else{
+                // Get the Mobile Service Table instance to use
+                mGestoreTable = mClient.getTable("gestore", Gestore.class);
+                //controllo se i dati inseriti sono corretti e se esiste il gestore con email e password inserite negli appositi campi
+                controlloDatiLoginGestore(email, password);
+            }
         } catch (MalformedURLException e) {
             e.printStackTrace();
         } catch (Exception e){
@@ -77,10 +79,10 @@ public class LoginController {
     /*
     esegue una ricerca in base alla email e alla password inserite dall'utente
      */
-    public List<Utente> riceviInfoUtenti(String email,String password)throws MobileServiceException,ExecutionException,InterruptedException{
+    public List<Utente> riceviInfoUtente(String email,String password)throws MobileServiceException,ExecutionException,InterruptedException{
         return mUtenteTable.where().field("email_u").eq(email).and().field("password_u").eq(password).execute().get();}
 
-    public void controlloDatiLogin(final String email, final String password){
+    public void controlloDatiLoginUtente(final String email, final String password){
         AsyncTask<Void, Void, Integer> task = new AsyncTask<Void, Void, Integer>(){
 
             ProgressDialog dialog;
@@ -98,7 +100,7 @@ public class LoginController {
             protected Integer doInBackground(Void... params) {
 
                 try {
-                    final List<Utente> results = riceviInfoUtenti(email,password);
+                    final List<Utente> results = riceviInfoUtente(email,password);
                     if(results.isEmpty()){
                         return 1;
                     }else{
@@ -130,6 +132,66 @@ public class LoginController {
 
         runAsyncTask(task);
     }
+
+
+    /*
+        esegue una ricerca in base alla email e alla password inserite dal gestore
+    */
+    public List<Gestore> riceviInfoGestore(String email,String password)throws MobileServiceException,ExecutionException,InterruptedException{
+        return mGestoreTable.where().field("email_g").eq(email).and().field("password_g").eq(password).execute().get();}
+
+    public void controlloDatiLoginGestore(final String email, final String password){
+        AsyncTask<Void, Void, Integer> task = new AsyncTask<Void, Void, Integer>(){
+
+            ProgressDialog dialog;
+
+            @Override
+            protected void onPreExecute() {
+                super.onPreExecute();
+                dialog = new ProgressDialog(context);
+                dialog.setMessage("Please wait...");
+                dialog.setIndeterminate(true);
+                dialog.show();
+            }
+
+            @Override
+            protected Integer doInBackground(Void... params) {
+
+                try {
+                    final List<Gestore> results = riceviInfoGestore(email,password);
+                    if(results.isEmpty()){
+                        return 1;
+                    }else{
+                        return 2;
+                    }
+                } catch (final Exception e){
+                    e.printStackTrace();
+                }
+                return null;
+            }
+
+            @Override
+            protected void onPostExecute(Integer b) {
+                super.onPostExecute(b);
+                if (dialog.isShowing()) {
+                    dialog.dismiss();
+                }
+
+                if(b==1){
+                    Toast toast = Toast.makeText(context,"l'username o la password inserite sono errate",Toast.LENGTH_LONG);
+                    toast.show();
+                }else{
+                    Intent intent= new Intent(context,HomeActivity.class).setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                    context.startActivity(intent);
+                }
+
+            }
+        };
+
+        runAsyncTask(task);
+    }
+
+
 
     /**
      * Run an ASync task on the corresponding executor
